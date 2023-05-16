33436c7c9adb83a12de7e276dcc6684f42cb19e1
Kolesanencore <kolesanencore@gmail.com>
2023-05-16 17:02:47 +0300

    Added early return pattern

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,18 +15,21 @@ export class App extends Component {
     const existingContact = this.state.contacts.find(
       contact => contact.name.toLowerCase() === name.toLowerCase()
     );
+
     if (existingContact) {
       alert(`${name} is already in contacts.`);
-    } else {
-      const newContact = {
-        id: nanoid(),
-        name: name,
-        number: number,
-      };
-      this.setState(prevState => ({
-        contacts: [...prevState.contacts, newContact],
-      }));
+      return;
     }
+
+    const newContact = {
+      id: nanoid(),
+      name: name,
+      number: number,
+    };
+
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, newContact],
+    }));
   };
 
   handleFilterChange = event => {
